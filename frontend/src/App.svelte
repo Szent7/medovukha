66772b2e21a2e3ee18c20802c1bf020e087247ef
Szent7 <a.vasilkov04@yandex.ru>
2024-12-06 17:55:52 +0500

    Frontend chore: Update dependencies refactor: Migrate to Svelte 5 feat: Add SPA-routing App.svelte now contains only the router. The pages have been moved to /routes. Materials from /templates are used as the pages layout

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,68 +1,12 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "./assets/vite.svg";
-  import Counter from "./lib/Counter.svelte";
-  import Testf from "./templates/test.svelte";
-  import Header from "./templates/header.svelte";
+  import { Router, Route } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import Containers from "./routes/Containers.svelte";
 
-  let choose: never;
+  export let url = "";
 </script>
 
-<Header />
-<main id="mApp">
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter bind:choose />
-    <p>{choose}</p>
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-
-  <div class="addit">
-    {#if choose == 0}
-      <Testf />
-    {/if}
-  </div>
-</main>
-
-<style>
-  #mApp {
-    max-width: 1280px;
-    margin: 0 auto;
-    padding: 2rem;
-    text-align: center;
-  }
-
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<Router {url}>
+  <Route path="/" component={Home} />
+  <Route path="/containers" component={Containers} />
+</Router>
--- frontend/src/templates/rowdiv.svelte
+++ frontend/src/templates/rowdiv.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
     import logo from "../assets/logo.svg";
-
-    let { content } = $props();
 </script>
 
 <div class="header">
     <img class="logoHeader" src={logo} alt="logo" />
-    {@render content?.()}
 </div>
 
 <style>
     .logoHeader {
         display: block;
-        max-height: 100px;
+        max-height: 100%;
     }
 
     .header {
+        height: 100px;
         -webkit-box-shadow: 0px 10px 10px 0px rgba(0, 0, 0, 0.2);
         -moz-box-shadow: 0px 10px 10px 0px rgba(0, 0, 0, 0.2);
         box-shadow: 0px 10px 10px 0px rgba(0, 0, 0, 0.2);
