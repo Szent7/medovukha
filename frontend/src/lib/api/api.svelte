ebcb28f22b26318b88dfbf1bafdaad4231821089
Szent7 <a.vasilkov04@yandex.ru>
2024-12-18 22:54:24 +0500

    feat(frontend-api):  make API requests asynchronous and add Zod lib to type JSON responses
    API requests are now in asynchronous functions and use zod schemes to type raw JSON

--- frontend/src/lib/api/api.svelte
+++ frontend/src/lib/api/api.svelte
@@ -1,7 +1,28 @@
-<script lang="ts">
+<script lang="ts" context="module">
     import axios from "axios";
+    import {
+        ContainerBaseInfoScheme,
+        type ContainerBaseInfo,
+    } from "./types.svelte";
 
-    axios.get("http://localhost:10015/ping").then(function (response) {
-        console.log(response.data);
-    });
+    export async function PingBack() {
+        axios.get("http://localhost:10015/ping").then(function (response) {
+            console.log(response.data);
+        });
+    }
+
+    export async function GetContainerList() {
+        try {
+            const response = await axios.get(
+                "http://localhost:10015/rest/v1/getcontainerlist",
+            );
+            const containers: ContainerBaseInfo = ContainerBaseInfoScheme.parse(
+                response.data,
+            );
+            console.log("responseData:" + containers);
+            return containers;
+        } catch (error) {
+            console.error("Error GET ContainerList:", error);
+        }
+    }
 </script>
---
+++ frontend/src/lib/api/types.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" context="module">
+    import { z } from "zod";
+
+    export const ContainerBaseInfoScheme = z.array(
+        z.object({
+            id: z.string(),
+            Names: z.array(z.string()),
+            Image: z.string(),
+            Ports: z.array(
+                z.object({
+                    IP: z.string(),
+                    PrivatePort: z.number(),
+                    PublicPort: z.number(),
+                    Type: z.string(),
+                }),
+            ),
+            Created: z.number(),
+            State: z.string(),
+        }),
+    );
+    export type ContainerBaseInfo = z.infer<typeof ContainerBaseInfoScheme>;
+</script>
