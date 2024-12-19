6ede7dde1a45490367d67ebe18885789149c7d20
Szent7 <a.vasilkov04@yandex.ru>
2024-12-19 19:45:39 +0500

    ✨feat(frontend-pages): add container control buttons
    add a selection system that applies actions to selected containers

---
+++ frontend/src/lib/containerActions.svelte
@@ -0,0 +1,39 @@
+<script lang="ts" context="module">
+    import {
+        KillContainerById,
+        PauseContainerById,
+        StartContainerById,
+        UnpauseContainerById,
+        RemoveContainerById,
+    } from "./api/api.svelte";
+
+    export function StartContainer(ids: string[]) {
+        ids.forEach((id: string): void => {
+            StartContainerById(id);
+        });
+    }
+
+    export function PauseContainer(ids: string[]) {
+        ids.forEach((id: string): void => {
+            PauseContainerById(id);
+        });
+    }
+
+    export function UnpauseContainer(ids: string[]) {
+        ids.forEach((id: string): void => {
+            UnpauseContainerById(id);
+        });
+    }
+
+    export function KillContainer(ids: string[]) {
+        ids.forEach((id: string): void => {
+            KillContainerById(id);
+        });
+    }
+
+    export function RemoveContainer(ids: string[]) {
+        ids.forEach((id: string): void => {
+            RemoveContainerById(id);
+        });
+    }
+</script>
--- frontend/src/routes/Containers.svelte
+++ frontend/src/routes/Containers.svelte
@@ -7,11 +7,29 @@
     import { GetContainerList } from "../lib/api/api.svelte";
     import type { ContainerBaseInfo } from "../lib/api/types.svelte";
     import { UnixTimeFormat } from "../lib/time.svelte";
+    import {
+        KillContainer,
+        PauseContainer,
+        RemoveContainer,
+        StartContainer,
+        UnpauseContainer,
+    } from "../lib/containerActions.svelte";
 
     let conList: ContainerBaseInfo = [];
     let loading = true;
+    let selectedIds: string[] = [];
+    const buttonIds: string[] = [
+        "start-button",
+        "stop-button",
+        "kill-button",
+        "restart-button",
+        "pause-button",
+        "resume-button",
+        "remove-button",
+    ];
+    let activateButtons: boolean = false;
 
-    onMount(() => {
+    const updateContainerList = () => {
         const getData = async () => {
             try {
                 const data = await GetContainerList();
@@ -27,7 +45,52 @@
             }
         };
         getData();
-    });
+    };
+
+    onMount(updateContainerList);
+
+    function updateSelected(id: string, checked: boolean) {
+        if (checked) {
+            selectedIds = [...selectedIds, id];
+            if (!activateButtons) {
+                buttonIds.forEach((item: string): void => {
+                    const button = document.getElementById(
+                        item,
+                    ) as HTMLButtonElement | null;
+                    if (button !== null) {
+                        button.disabled = false;
+                    }
+                });
+                activateButtons = true;
+            }
+        } else {
+            selectedIds = selectedIds.filter((item) => item !== id);
+            if (selectedIds.length == 0) {
+                buttonIds.forEach((item: string): void => {
+                    const button = document.getElementById(
+                        item,
+                    ) as HTMLButtonElement | null;
+                    if (button !== null) {
+                        button.disabled = true;
+                    }
+                });
+                activateButtons = false;
+            }
+        }
+        console.log("SelectedIds:" + selectedIds);
+    }
+
+    function selectAll(checked: boolean) {
+        const elements = document.querySelectorAll("input[name=checkbox-item]");
+        if (elements !== null) {
+            Array.prototype.forEach.call(elements, function (item) {
+                if (item.checked != checked) {
+                    updateSelected(item.id, checked);
+                    item.checked = checked;
+                }
+            });
+        }
+    }
 </script>
 
 <Sidebar />
@@ -36,11 +99,14 @@
 {#snippet content()}
     <FrameElement content={header} />
     {#if loading}
-        <p>Loading data...</p>
+        <FrameElement content={loadingData} />
     {:else if !loading}
         <FrameElement content={containerTable} />
     {/if}
-    <!--<FrameElement content={containerTable} />-->
+{/snippet}
+
+{#snippet loadingData()}
+    <p>Loading data...</p>
 {/snippet}
 
 {#snippet header()}
@@ -49,10 +115,91 @@
 {/snippet}
 
 {#snippet containerTable()}
+    <div class="button-block">
+        <button
+            class="containerlist-button"
+            id="start-button"
+            onclick={() => {
+                StartContainer(selectedIds);
+                setTimeout(() => {
+                    updateContainerList();
+                }, 1000);
+            }}
+            disabled>Start</button
+        >
+        <button
+            class="containerlist-button"
+            id="stop-button"
+            onclick={() => {
+                //StopContainer(selectedIds);
+            }}
+            disabled>Stop</button
+        >
+        <button
+            class="containerlist-button"
+            id="kill-button"
+            onclick={() => {
+                KillContainer(selectedIds);
+                setTimeout(() => {
+                    updateContainerList();
+                }, 1000);
+            }}
+            disabled>Kill</button
+        >
+        <button
+            class="containerlist-button"
+            id="restart-button"
+            onclick={() => {
+                //RestartContainer(selectedIds);
+            }}
+            disabled>Restart</button
+        >
+        <button
+            class="containerlist-button"
+            id="pause-button"
+            onclick={() => {
+                PauseContainer(selectedIds);
+                setTimeout(() => {
+                    updateContainerList();
+                }, 1000);
+            }}
+            disabled>Pause</button
+        >
+        <button
+            class="containerlist-button"
+            id="resume-button"
+            onclick={() => {
+                UnpauseContainer(selectedIds);
+                setTimeout(() => {
+                    updateContainerList();
+                }, 1000);
+            }}
+            disabled>Resume</button
+        >
+        <button
+            class="containerlist-button"
+            id="remove-button"
+            onclick={() => {
+                RemoveContainer(selectedIds);
+                setTimeout(() => {
+                    updateContainerList();
+                }, 1000);
+            }}
+            disabled>Remove</button
+        >
+    </div>
     <table class="containerlist-table">
         <thead>
             <tr>
-                <th><input type="checkbox" /></th>
+                <th
+                    ><input
+                        type="checkbox"
+                        onchange={(event) => {
+                            const target = event.target as HTMLInputElement;
+                            selectAll(target.checked);
+                        }}
+                    /></th
+                >
                 <th>Name</th>
                 <th>State</th>
                 <th>Image</th>
@@ -63,13 +210,32 @@
         <tbody>
             {#each conList as container}
                 <tr>
-                    <td><input type="checkbox" /></td>
+                    <td
+                        ><input
+                            type="checkbox"
+                            name="checkbox-item"
+                            id={container.id}
+                            onchange={(event) => {
+                                const target = event.target as HTMLInputElement;
+                                updateSelected(container.id, target.checked);
+                            }}
+                        /></td
+                    >
                     <td>{container.Names[0]}</td>
-                    <td>{container.State}</td>
+                    <td class="state-{container.State}">{container.State}</td>
                     <td>{container.Image}</td>
                     <td
-                        >{container.Ports[0].PublicPort}:{container.Ports[0]
-                            .PrivatePort}</td
+                        >{#if container.Ports.length == 0}
+                            -
+                        {:else}
+                            {#each container.Ports as port}
+                                {#if port?.PublicPort !== undefined && port?.PrivatePort !== undefined}
+                                    <p>{port.PublicPort}:{port.PrivatePort}</p>
+                                {:else}
+                                    -
+                                {/if}
+                            {/each}
+                        {/if}</td
                     >
                     <td>{UnixTimeFormat(container.Created)}</td>
                 </tr>
@@ -87,5 +253,25 @@
     .containerlist-table {
         width: 100%;
         text-align: center;
+    }
+
+    .containerlist-button {
+        border-radius: 10px;
+    }
+
+    .button-block {
+        text-align: right;
+    }
+
+    .state-running {
+        color: aquamarine;
+    }
+
+    .state-exited {
+        color: red;
+    }
+
+    .state-paused {
+        color: orange;
     }
 </style>
